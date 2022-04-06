914f3006ed2da7ef49e311175a4bbf3d2f297696
Nader-Opeyany <72956563+Nader-Opeyany@users.noreply.github.com>
2022-04-05 17:59:41 -0700

    final

--- fancifyMytext.js
+++ fancifyMytext.js
@@ -11,8 +11,40 @@ function biggerIsBetter(){
 function changeText(){
     var mess = document.getElementById("a");
     mess.style.fontWeight = "bold";
-}
-function changeBack(event){
+    mess.style.color = "blue";
+    mess.style.textDecoration = "underline"}
+function changeBack(){
     var mess = document.getElementById("a");
     mess.style.fontWeight = "normal";
+    mess.style.color = "black";
+    mess.style.textDecoration = "none"}
+
+/*function moo(){
+    var mess = document.getElementById("a");
+    mess.value = mess.value.toUpperCase();
+    size = mess.value.length;
+    for(var i = 0; i<size;i++){
+        console.log(size + "size");
+        if(mess.value[i]=="." || mess.value[i] == "?" || mess.value[i] == "!"){
+            var period = mess.value[i].split(".");
+            //the text before puntuation has -moo added
+            period[i - 1] += "-MOO";
+            console.log(period.value.length + "size");
+
+            i += 4;//i skips 4
+            size += 4;//size skips 4
+            mess.value = period.join(" ")
+        }
+    }
+}*/
+
+function moo(){
+var mess = document.getElementById("a");
+mess.value = mess.value.toUpperCase() 
+var words = mess.value.split(".")
+words.pop();
+for(var i=0;i<words.length;i++){
+    words[i] += "-MOO";
+    }
+mess.value = words.join(" ");
 }
