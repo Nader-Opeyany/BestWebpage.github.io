4e340b31969880418184952cfd25be39b216fc8b
Nader Opeyany <naderopeyany@gmail.com>
2022-05-02 19:04:56 -0700

    assignment 5

--- assignment 5/encryptIt.js
+++ assignment 5/encryptIt.js
@@ -17,8 +17,36 @@
     function init() {
       // Note: In this function, we usually want to set up our event handlers
       // for UI elements on the page.
+      console.log("Window loaded!");
+      document.getElementById("encrypt-it").addEventListener("click",handleclick);
+
     }
-  
+
+    function handleclick(){
+      var text = document.getElementById("input-text").value;
+      console.log(document.getElementById("input-text").value);
+      console.log(shiftCipher(text))
+      document.getElementById("input-text").value = shiftCipher(text)
+    }
+    
+    function shiftCipher(text) {
+      //console.log("here's this" + text)
+      text = text.toLowerCase();
+      //console.log(text)
+      let result = "";
+      for (let i = 0; i < text.length; i++) {
+        if (text[i] < 'a' || text[i] > 'z') {
+          result += text[i];
+        } else if (text[i] == 'z') {
+          result += 'a';
+        } else { // letter is between 'a' and 'y'
+          let letter = text.charCodeAt(i);
+          let resultLetter = String.fromCharCode(letter + 1);
+          result += resultLetter;
+        }
+      }
+      return result;
+    }
     // Add any other functions in this area (you should not implement your
     // entire program in the init function, for similar reasons that
     // you shouldn't write an entire Java program in the main method).
